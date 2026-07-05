99154b64683110f5c950850f2d2b3abf1e70201a
znesr99 <ynp98t4gm9@privaterelay.appleid.com>
2026-07-05 17:00:09 +0700

    Update AllMAP.lua

--- AllMAP.lua
+++ AllMAP.lua
@@ -1,1 +1,13 @@
+getgenv().Map = {
+	["Rock Fruit"] = {
+		ID = 119091355492870,
+		HTTP = "https://raw.githubusercontent.com/znesr99/MarvenRizHub/refs/heads/main/Rock_Fruit.lua"
+	},
+}
 
+for _, v in pairs(getgenv().Map) do
+    if game.PlaceId == v.ID then
+        loadstring(game:HttpGet(v.HTTP))()
+        break
+    end
+end
