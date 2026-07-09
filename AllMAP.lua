1b4aa2f70b68c799ec96a403156c83d7611a9501
znesr99 <ynp98t4gm9@privaterelay.appleid.com>
2026-07-09 23:08:40 +0700

    Update AllMAP.lua

--- AllMAP.lua
+++ AllMAP.lua
@@ -3,7 +3,12 @@ getgenv().Map = {
 		ID = 119091355492870,
 		HTTP = "https://raw.githubusercontent.com/znesr99/MarvenRizHub/refs/heads/main/Rock_Fruit.lua"
 	},
+	["Reign piece"] = {
+		ID = 78466992256287,
+		HTTP = "https://github.com/znesr99/MarvenRizHub/blob/main/Reign_piece.lua"
+	},
 }
+
 
 for _, v in pairs(getgenv().Map) do
     if game.PlaceId == v.ID then
