d85f924bfdae5f46749d120eb4a968d0db9e4948
znesr99 <ynp98t4gm9@privaterelay.appleid.com>
2026-07-16 23:19:29 +0700

    Update AllMAP.lua

--- AllMAP.lua
+++ AllMAP.lua
@@ -2,15 +2,19 @@ getgenv().Map = {
 	["Rock Fruit"] = {
 		ID = 119091355492870,
 		HTTP = "https://raw.githubusercontent.com/znesr99/MarvenRizHub/refs/heads/main/Rock_Fruit.lua"
-	},
+	};
 	["Reign piece"] = {
 		ID = 78466992256287,
 		HTTP = "https://raw.githubusercontent.com/znesr99/MarvenRizHub/refs/heads/main/Reign_piece.lua"
-	},
+	};
 	["Legacy piece"] = {
 		ID = 111097829542198,
 		HTTP = "https://raw.githubusercontent.com/znesr99/MarvenRizHub/refs/heads/main/Legacy_piece.lua"
-	},
+	};
+	["1.Request : Primal Pursuit"] = {
+		ID = 12076775711,
+		HTTP = "https://raw.githubusercontent.com/znesr99/MarvenRizHub/refs/heads/main/1Request_Primal_Pursuit.lua"
+	};
 }
 
 for _, v in pairs(getgenv().Map) do
@@ -19,4 +23,3 @@ for _, v in pairs(getgenv().Map) do
         break
     end
 end
-
