6d06445d332fdacf72d3067d6a55dade02e86fb8
znesr99 <ynp98t4gm9@privaterelay.appleid.com>
2026-09-19 12:54:14 +0700

    Fix HTTP links for Rock Fruit and Rock Fruit Raid

--- AllMAP.lua
+++ AllMAP.lua
@@ -1,11 +1,11 @@
 getgenv().Map = {
 	["Rock Fruit"] = {
 		ID = 119091355492870,
-		HTTP = "https://raw.githubusercontent.com/znesr99/MarvenRizHub/refs/heads/main/Rock_Fruit_Main"
+		HTTP = "https://raw.githubusercontent.com/znesr99/MarvenRizHub/refs/heads/main/Rock_Fruit_Main.lua"
 	};
     ["Rock Fruit Raid"] = {
 		ID = 82878101790702,
-		HTTP = "https://raw.githubusercontent.com/znesr99/MarvenRizHub/refs/heads/main/Rock_Fruit_Main"
+		HTTP = "https://raw.githubusercontent.com/znesr99/MarvenRizHub/refs/heads/main/Rock_Fruit_Main.lua"
 	};
 	["Reign piece"] = {
 		ID = 78466992256287,
