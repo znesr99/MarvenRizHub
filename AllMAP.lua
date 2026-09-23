08a4cdd4777125f607ff35efdfec9c583f1bc66d
znesr99 <ynp98t4gm9@privaterelay.appleid.com>
2026-09-23 16:18:26 +0700

    Update HTTP link for Rock Fruit in AllMAP.lua

--- AllMAP.lua
+++ AllMAP.lua
@@ -1,7 +1,7 @@
 getgenv().Map = {
 	["Rock Fruit"] = {
 		ID = 119091355492870,
-		HTTP = "https://raw.githubusercontent.com/znesr99/MarvenRizHub/refs/heads/main/Rock_Fruit_Main.lua"
+		HTTP = "https://vault.pandauth.com/vss/c66898e1d330931d"
 	};
     ["Rock Fruit Raid"] = {
 		ID = 82878101790702,
